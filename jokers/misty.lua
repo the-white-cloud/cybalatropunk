19e126e3d1345407e4134929546bc25a07205fc0
jules <k23157161@kcl.ac.uk>
2025-06-14 19:04:51 +0100

    still jank and even more broken

---
+++ jokers/misty.lua
@@ -0,0 +1,47 @@
+-- region Misty
+
+local misty = {
+	name = "Misty",
+	key = "j_cbpunk_Misty",
+	config = {extra = {Xmult = 1, Xmult_scale = 0.25}},
+	pos = {x = 0, y = 0},
+	rarity = 3,
+	cost = 6,
+	discovered = true,
+	blueprint_compat = true,
+	eternal_compat = true,
+	perishable_compat = true,
+	atlas = "j_cbpunk_jokers",
+    credit = {
+		art = "Jules (the-white-cloud)",
+		code = "Jules (the-white-cloud)",
+		concept = "Jules (the-white-cloud)"
+	},
+    description = "Gains +0.25 XMult per tarot card used this run."
+}
+
+misty.loc_vars = function(self, info_queue, card)
+    return {vars = {card.ability.extra.Xmult, card.ability.extra.Xmult_scale}}
+end
+
+misty.calculate = function(self, card, context)
+    if context.using_consumeable and not context.blueprint and (context.consumeable.ability.set == "Tarot") then
+        card.ability.extra.XMult = 1 + card.ability.extra.Xmult_scale * G.GAME.consumeable_usage_total.tarot
+    end
+    if context.joker_main then
+        local c = 1 + card.ability.extra.Xmult_scale * G.GAME.consumeable_usage_total.tarot
+        if c ~= 1 then
+            return {
+                card = card,
+                Xmult_mod = c,
+                message = 'x' .. c,
+                colour = G.C.MULT
+            }
+        end
+    end
+end
+
+
+
+SMODS.Joker(misty)
+-- endregion Misty
