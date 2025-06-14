776d565e61a38b097b7b9ea7683a2475cbb43f5c
jules <k23157161@kcl.ac.uk>
2025-06-14 23:49:09 +0100

    Added JokerDisplay support

--- jokers/misty.lua
+++ jokers/misty.lua
@@ -44,6 +44,19 @@ SMODS.Joker{
                 }
             end
         end
+    end,
+    joker_display_def = function(JokerDisplay)
+        ---@type JDJokerDefinition
+        return {
+            text = {
+                {
+                    border_nodes = {
+                        { text = "X" },
+                        { ref_table = "card.ability", ref_value = "x_mult", retrigger_type = "exp" }
+                    }
+                }
+            }
+        }
     end
 }
 -- endregion Misty
