1a54a66b6127dd90a499f8d2272a01afa48a0d05
jules <k23157161@kcl.ac.uk>
2025-06-28 00:23:01 +0100

    temp safe still v broken

--- jokers/brendan.lua
+++ jokers/brendan.lua
@@ -3,7 +3,12 @@ SMODS.Joker{
 	-- name = "Brendan",
 	key = "Brendan",
 	atlas = "j_cbpunk_jokers",
-	config = {extra = 4,},
+	config = {
+		extra = {
+			chance = 4,
+			round = 0,
+		},
+	},
 	pos = {x = 2, y = 0},
     pixel_size = { w = 71, h = 95},
 	rarity = 3,
@@ -19,10 +24,21 @@ SMODS.Joker{
 	},
     description = "1 in 4 chance for a booster pack to be free.",
     loc_vars = function(self, info_queue, card)
-        return {vars = {card.ability.x_mult, card.ability.extra}}
+        return {vars = {card.ability.extra.chance}}
     end,
 
     calculate = function(self, card, context)
+		if G.shop and (card.ability.extra.round ~= G.round) then
+			card.ability.extra.round = G.round
+			for k, v in pairs(G.shop_booster.cards) do
+                if (math.random(card.ability.extra.chance) == 1) then
+					v.ability.couponed = true
+					v:set_cost()
+					card_eval_status_text(card, 'extra chance', 0, nil, nil, {message = "Take this!"})
+				end
+            end
+		end
+		
     end
 }
 -- endregion Brendan
